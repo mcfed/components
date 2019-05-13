8a0fd357263d4566204449a95f619d595a4e8d11
guor <guor@mczh.com.cn>
2019-05-13 16:35:52 +0800

    advancedSearch.stories修改

--- modules/AdvancedSearch/__stories__/advancedSearch.stories.js
+++ modules/AdvancedSearch/__stories__/advancedSearch.stories.js
@@ -7,17 +7,11 @@ import { linkTo } from '@storybook/addon-links'
 import { withTests } from '@storybook/addon-jest'
 import { Button, Welcome } from '@storybook/react/demo'
 
-import AdvancedSearch from '../src/components/AdvancedSearch'
+import AdvancedSearch from '../AdvancedSearch'
 // import results from '../.jest-test-results.json';
 
-// addDecorator(
-//   withTests({
-//     results,
-//     filesExt: '((\\.specs?)|(\\.tests?))?(\\.ts)?$',
-//   })
-// );
 storiesOf("搜索组件包装AdvancedSearch",module)
-  // .addParameters({ jest: ['../src/components/AdvancedSearch/__tests__/AdvancedSearch.spec.js'] })
+
   .add('advancedSearch',()=>
     <AdvancedSearch filterSubmitHandler={action('button-click')}>
       <Input name="callState" label="呼叫类型"  />
