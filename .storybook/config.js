2ac3bf35189d6e3ef8192a52a6e308ae402af7b3
jaxchow <jaxchow@gmail.com>
2019-05-14 16:50:12 +0800

    更新代码错误

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,7 @@ import { withConsole } from '@storybook/addon-console'//console
 import { withA11y } from '@storybook/addon-a11y'
 import { addParameters } from '@storybook/react'
 import { withTests } from '@storybook/addon-jest'
-const { withPropsTable } from 'storybook-addon-react-docgen'
+import { withPropsTable } from 'storybook-addon-react-docgen'
 import results from '../jest-test-results.json'
 import '../src/index.less'
 const newViewports = {
@@ -28,7 +28,7 @@ function loadStories() {
   require('../stories/index.js');
   // require('../stories/panel.stories.js');
   require('../packages/components/modules/AdvancedSearch/__stories__/advancedSearch.stories.js')
-  require('../packages/components/modules/Panel/__stories__/Panel.stories.js')
+  // require('../packages/components/modules/Panel/__stories__/Panel.stories.js')
   // You can require as many stories as you need.
 }
 
