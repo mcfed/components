2d4f5c5cc8d9622ebe9bf2ffbd4e60fd769508db
guor <guor@mczh.com.cn>
2019-05-14 15:53:14 +0800

    advancedSearch.stories中加入单元测试和md文档

--- modules/AdvancedSearch/__stories__/advancedSearch.stories.js
+++ modules/AdvancedSearch/__stories__/advancedSearch.stories.js
@@ -2,17 +2,20 @@
 import React from 'react'
 import {Input,Select} from 'antd'
 import { storiesOf  } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-
+import { withKnobs } from '@storybook/addon-knobs';
 import AdvancedSearch from '../AdvancedSearch'
+import Readme from '../index.md';
+
+stories.addDecorator(withKnobs);
+stories.addParameters({ jest: ['AdvancedSearch.spec.js'] })
 
 storiesOf("查询栏组件",module)
   .add('基础组件',()=>
     <AdvancedSearch filterSubmitHandler={action('button-click')}>
       <Input name="name" label="姓名"  />
       <Select name="type" label="类型" />
-    </AdvancedSearch>
+    </AdvancedSearch>,
+   { notes: { markdown: Readme }}
   )
   .add('基础组件--3个以上查询条件时出现扩展',()=>
     <AdvancedSearch filterSubmitHandler={action('button-click')}>
@@ -20,5 +23,6 @@ storiesOf("查询栏组件",module)
       <Select name="type" label="类型"  />
       <Select name="level" label="级别" />
       <Select name="sex" label="性别" />
-    </AdvancedSearch>
+    </AdvancedSearch>,
+   { notes: { markdown: Readme }}
   )
