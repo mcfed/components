87769a9b0941b2ea809ff04ab78ba041b949c1d3
lujianan <>
2019-06-18 10:19:55 +0800

    transfer增加几个key，selectData

--- modules/TransferView/__stories__/transferView.stories.js
+++ modules/TransferView/__stories__/transferView.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Input, Select } from "antd";
+import { Input, Select, Button } from "antd";
 import { storiesOf } from "@storybook/react";
-import AdvancedSearch from "../index";
+import McTransfer from "../index";
 import { actions } from "@storybook/addon-actions";
 // import Readme from "../README.md";
 
@@ -16,59 +16,61 @@ for (let i = 0; i < 20; i++) {
     key: i.toString(),
     title: `content${i + 1}`,
     description: `description of content${i + 1}`,
-    name: this.getName(`name${i + 1}`,i),
-    chosen: Math.random() * 2 > 1,
+    name: `name${i + 1}`,
+    chosen: Math.random() * 2 > 1
   };
   if (data.chosen) {
     targetKeys.push(data.key);
   }
   mockData.push(data);
 }
-const header = [{
-  text: 'title',
-  width: 160
-},{
-  text: 'description',
-  width: 80
-},{
-  text: 'name',
-  width: 60
-}]
-const searchItem = 'title'
+const header = [
+  {
+    text: "title",
+    width: 160
+  },
+  {
+    text: "description",
+    width: 80
+  },
+  {
+    text: "name",
+    width: 60
+  }
+];
+const searchItem = "title";
 
-function handleChange(targetKeys, direction, moveKeys){
+function handleChange(targetKeys, direction, moveKeys) {
   console.log(targetKeys, direction, moveKeys);
   // targetKeys = targetKeys
 }
 
-function handleSearch(dir, value){
-  console.log('search:', dir, value);
-};
+function handleSearch(dir, value) {
+  console.log("search:", dir, value);
+}
 
-renderFooter = () => (
-  <Button
-    size="small"
-    style={{ float: 'right', margin: 5 }}
-    // onClick={getMock}
-  >
-      reload
-  </Button>
-)
-stories.add(
-  "基础用法",
-  () => (
-    <McTransfer
-      dataSource={mockData}
-      // listStyle={listStyle}
-      targetKeys={targetKeys}
-      onChange={handleChange}
-      header={header}
-      titles={['Source', 'Target']}
-      showSearch={true}
-      onSearch={handleSearch}
-      searchItem={searchItem}
-      footer={renderFooter}
-    />
-  ),
-  { notes: { markdown: Readme } }
-);
+// renderFooter = () => (
+//   <Button size="small" style={{ float: "right", margin: 5 }}>
+//     reload
+//   </Button>
+// );
+stories.add("基础用法", () => (
+  <McTransfer
+    dataSource={mockData}
+    targetKeys={targetKeys}
+    onChange={handleChange}
+    header={header}
+    titles={["Source", "Target"]}
+    showSearch={true}
+    onSearch={handleSearch}
+    filterOption={function(filter, item) {
+      return (
+        (filter.input ? item.title.indexOf(filter.input) >= 0 : true) &&
+        (filter.select ? item.key === filter.select : true)
+      );
+      // && item.key === filter.select
+    }}
+    searchItem={searchItem}
+    // footer={renderFooter}
+  />
+));
