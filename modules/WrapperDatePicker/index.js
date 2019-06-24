4600639689c7a405d1f0b8bb5963b97c94f5d8b0
shibo <shoes@shibodeMacBook-Pro.local>
2019-06-24 17:34:27 +0800

    :bug: bug fixed

--- modules/WrapperDatePicker/index.js
+++ modules/WrapperDatePicker/index.js
@@ -2,72 +2,46 @@ import React, { Component } from "react";
 import moment from "moment";
 import PropTypes from "prop-types";
 
-export default class WrapperDatePicker extends Component{
-
-  // constructor(props){
-  //   super(props)
-  //   if(props.value instanceof Array){
-  //     this.state={
-  //       value: (props.value && props.value.length==2) ? [new moment(props.value[0],props.format),new moment(props.value[1],props.format)]:null
-  //     }
-  //   }else{
-  //     this.state={
-  //       value: (props.value && props.value!== "") ? new moment(props.value,props.format):null
-  //     }
-  //   }
-  // }
-  constructor(props){
-    super(props)
-    this.translateVal2State(props.value,props.format)
-    // if(props.value instanceof Array){
-    //   this.state={
-    //     value: (props.value && props.value.length==2) ? [ moment(moment(props.value[0]).format(props.format)),moment(moment(props.value[1]).format(props.format))]:null
-    //   }
-    // }else{
-    //   this.state={
-    //     value: (props.value && props.value!== "") ? new moment(props.value,props.format):null
-    //   }
-    // }
-  }
-
-  // componentWillReceiveProps(nextProps){
-  //   if(JSON.stringify(nextProps.value)!==JSON.stringify(this.props.value)){
-  //     if(nextProps.value instanceof Array){
-  //       console.log(nextProps.value)
-  //       this.setState({
-  //         value: (nextProps.value && nextProps.value.length==2 && nextProps.value[0]!=="" && nextProps.value[1] !=="") ? [new moment(nextProps.value[0],nextProps.format),new moment(nextProps.value[1],nextProps.format)]:null
-  //       })
-  //     }else{
-  //       this.setState({
-  //         value: (nextProps.value && nextProps.value!== "") ? new moment(nextProps.value,nextProps.format):null
-  //       })
-  //     }
-  //   }
-  // }
-  componentWillReceiveProps(nextProps){
-    if(JSON.stringify(nextProps.value)!==JSON.stringify(this.props.value)){
-      this.translateVal2State(nextProps.value,nextProps.format)
-      // if(nextProps.value instanceof Array){
-      //   this.setState({
-      //     value: (nextProps.value && nextProps.value.length==2 && nextProps.value[0]!=="" && nextProps.value[1] !=="") ? [ moment(moment(nextProps.value[0]).format(nextProps.format)),moment(moment(nextProps.value[1]).format(nextProps.format))]:null
-      //   })
-      // }else{
-      //   this.setState({
-      //     value: (nextProps.value && nextProps.value!== "") ? new moment(nextProps.value,nextProps.format):null
-      //   })
-      // }
+export default class WrapperDatePicker extends Component {
+  constructor(props) {
+    super(props);
+    if(props.value instanceof Array){
+      this.state={
+        value: props.value && props.value.length == 2
+          ? [
+              moment(moment(props.value[0]).format(props.format)),
+              moment(moment(props.value[1]).format(props.format))
+            ]
+          : null
+      }
+    }else{
+      this.state={
+        value: (props.value && props.value!== "") ? new moment(props.value,props.format):null
+      }
     }
   }
 
-  translateVal2State (value,format) {
-    if(value instanceof Array){
+  componentWillReceiveProps(nextProps) {
+    if (JSON.stringify(nextProps.value) !== JSON.stringify(this.props.value)) {
+      this.translateVal2State(nextProps.value, nextProps.format);
+    }
+  }
+
+  translateVal2State(value, format) {
+    if (value instanceof Array) {
       this.setState({
-        value: (value && value.length==2) ? [ moment(moment(value[0]).format(format)),moment(moment(value[1]).format(format))]:null
-      })
-    }else{
+        value:
+          value && value.length == 2
+            ? [
+                moment(moment(value[0]).format(format)),
+                moment(moment(value[1]).format(format))
+              ]
+            : null
+      });
+    } else {
       this.setState({
-        value: (value && value!== "") ? new moment(value,format):null
-      })
+        value: value && value !== "" ? new moment(value, format) : null
+      });
     }
   }
 
