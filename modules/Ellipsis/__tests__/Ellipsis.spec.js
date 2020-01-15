b07dc3469fef04af0b2dae9a10161877cecc7746
guor@mchz.com.cn <guor@mchz.com.cn>
2020-01-15 10:16:07 +0800

    feat: :hammer: Ellipsis ts重构

--- modules/Ellipsis/__tests__/Ellipsis.spec.js
+++ modules/Ellipsis/__tests__/Ellipsis.spec.js
@@ -1,8 +1,7 @@
-import { shallow, mount, render } from "enzyme";
-import React from "react";
-import { Tooltip } from "antd";
+import {shallow, mount, render} from 'enzyme';
+import React from 'react';
 
-import Ellipsis from "../index";
+import Ellipsis from '../index.tsx';
 
 const setup = props => {
   const wrapper = shallow(<Ellipsis {...props} />);
@@ -13,38 +12,38 @@ const setup = props => {
   };
 };
 
-describe("Ellipsis render 是否正确渲染", () => {
+describe('Ellipsis render 是否正确渲染', () => {
   it("默认的tooltiptext为text 的默认值 ''", () => {
-    const { wrapper, props } = setup({});
+    const {wrapper, props} = setup({});
 
-    expect(wrapper.find("Tooltip").prop("title")).toBe("Ellipsis");
+    expect(wrapper.find('Tooltip').prop('title')).toBe('');
   });
 
-  it("若tooltiptext 为undefined text不传，则title默认值为Ellipsis", () => {
-    const { wrapper, props } = setup({
+  it('若tooltiptext 为undefined text不传，则title默认值为Ellipsis', () => {
+    const {wrapper, props} = setup({
       tooltiptext: undefined
     });
-    expect(wrapper.find("Tooltip").prop("title")).toBe("Ellipsis");
+    expect(wrapper.find('Tooltip').prop('title')).toBe('');
   });
 
-  it("tooltip 为被渲染的节点 ", () => {
-    const { wrapper, props } = setup({});
+  it('tooltip 为被渲染的节点 ', () => {
+    const {wrapper, props} = setup({});
 
-    expect(wrapper.find("Tooltip").exists()).toBe(true);
+    expect(wrapper.find('Tooltip').exists()).toBe(true);
   });
 
-  it("tooltiptext最终传值给 tooltip的title", () => {
-    const { wrapper, props } = setup({
-      tooltiptext: "aa123"
+  it('tooltiptext最终传值给 tooltip的title', () => {
+    const {wrapper, props} = setup({
+      tooltiptext: 'aa123'
     });
-    expect(wrapper.find("Tooltip").prop("title")).toBe(props.tooltiptext);
+    expect(wrapper.find('Tooltip').prop('title')).toBe(props.tooltiptext);
   });
 
-  it("若tooltiptext值为undefined，则tooltip的title为传入的text", () => {
-    const { wrapper, props } = setup({
-      text: "aa123",
+  it('若tooltiptext值为undefined，则tooltip的title为传入的text', () => {
+    const {wrapper, props} = setup({
+      text: 'aa123',
       tooltiptext: undefined
     });
-    expect(wrapper.find("Tooltip").prop("title")).toBe(props.text);
+    expect(wrapper.find('Tooltip').prop('title')).toBe(props.text);
   });
 });
