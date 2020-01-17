a9ea678ab0c8659ad0be8242a8bb6e599a1ec3d2
lius <liuswin@163.com>
2020-01-17 17:30:55 +0800

    refactor: ts 重构 collaspePanel

--- modules/CollapsePanel/index.tsx
+++ modules/CollapsePanel/index.tsx
@@ -10,17 +10,84 @@ interface CollapsePanelState {
 }
 
 interface CollapsePanelProps {
-  children?: React.ReactNode;
+  title: string;
+  renderable: boolean | Function;
+  control: React.ReactNode | React.ReactElement;
+  children: React.ReactNode;
+  closeValues?: [];
 }
 
-interface CollapsePanelP {}
+interface ICollapsePanel {
+  setActiveStatus(): void;
+  render(): React.ReactNode;
+}
 
-export default class CollapsePanel extends Component<
-  CollapsePanelProps,
-  CollapsePanelState
-> {
+export default class CollapsePanel
+  extends Component<CollapsePanelProps, CollapsePanelState>
+  implements ICollapsePanel {
+  componentDidMount() {
+    this.setActiveStatus();
+  }
+  setActiveStatus(): void {
+    const {control} = this.props;
+    this.setState({
+      active: this.isExtraIsReactDom(control) ? this.fieldValueChange() : true
+    });
+  }
+  fieldValueChange(): boolean {
+    const {formRef} = this.context;
+    const {control, closeValues} = this.props;
+    /**
+     * closeValues 关闭值数组【默认为空数组】
+     * 若closeValues 传入则判断值是否在该数组中 存在则返回false
+     * 若未传入则值转boolean
+     */
+    return closeValues.length
+      ? closeValues.filter(
+          it => it === formRef.getFieldValue(control.props.name)
+        ).length === 0
+      : Boolean(formRef.getFieldValue(control.props.name));
+  }
+  isExtraIsReactDom(extra): boolean {
+    return typeof extra === 'object' && typeof extra.$$typeof === 'symbol';
+  }
+  renderHeader(): React.ReactElement {
+    let {title} = this.props;
+    return (
+      <div className='CollapsePanel-header'>
+        <h5 className='CollapsePanel-title'>
+          {title}
+          <div className='CollapsePanel-extra'>{this.renderExtra()}</div>
+        </h5>
+      </div>
+    );
+  }
+  renderExtra() {
+    let {control} = this.props;
+    return this.isExtraIsReactDom(control)
+      ? React.createElement(FormItem, {}, control)
+      : control;
+  }
   render() {
-    const {children} = this.props;
-    return <div></div>;
+    const {children, renderable, ...otherProps} = this.props;
+    const {formRef} = this.context;
+    let {active} = this.state;
+    let renderProps = true;
+    if (
+      (typeof renderable === 'boolean' && renderable === false) ||
+      (typeof renderable === 'function' &&
+        renderable.apply(this, [formRef]) === false)
+    ) {
+      renderProps = false;
+    }
+    return renderProps ? (
+      <Panel
+        header={this.renderHeader()}
+        {...otherProps}
+        // @ts-ignore
+        isActive={children ? active : false}>
+        {children}
+      </Panel>
+    ) : null;
   }
 }
