3b1c6b126bf51018d0e3bf090f73306f63c41cf2
qianjiacheng <qianjiacheng@MacBook-Pro.local>
2020-03-25 14:48:34 +0800

    feat: 提供ts支持

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,13 @@ const storysource = require.resolve('@storybook/addon-storysource/loader')
 
 module.exports = function({ config }) {
   config.module.rules.push({
+    test: /\.tsx?$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]],
+    },
+  });
+  config.module.rules.push({
     test: /\.jsx?$/,
     loaders: [storysource],
     include: path.resolve(__dirname, '../'), // this fixed it, I think.
@@ -25,6 +32,7 @@ module.exports = function({ config }) {
       }
     }],
   });
+  config.resolve.extensions.push('.ts', '.tsx');
   config.resolve.alias = {
       'mcf-components':  path.resolve(__dirname,'../packages/components'),  
   };
