7015289d2bcaeffdee09a37f1783d5155936580f
Jshoes <576504437@qq.com>
2020-06-28 14:12:00 +0800

    feat: add config & ts-components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,30 +1,30 @@
-import { configure } from '@storybook/react'
-import { addDecorator } from '@storybook/react'
-import { withConsole } from '@storybook/addon-console'//console
-import { withA11y } from '@storybook/addon-a11y'
-import { addParameters } from '@storybook/react'
-import { withTests } from '@storybook/addon-jest'
-import { withPropsTable } from 'storybook-addon-react-docgen'
-import { withKnobs } from '@storybook/addon-knobs'
-import { withSmartKnobs } from 'storybook-addon-smart-knobs'
-import results from '../jest-test-results.json'
-import '../src/index.less'
+import { configure } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
+import { withConsole } from "@storybook/addon-console"; //console
+import { withA11y } from "@storybook/addon-a11y";
+import { addParameters } from "@storybook/react";
+import { withTests } from "@storybook/addon-jest";
+import { withPropsTable } from "storybook-addon-react-docgen";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withSmartKnobs } from "storybook-addon-smart-knobs";
+import results from "../jest-test-results.json";
+import "../src/index.less";
 const newViewports = {
-    kindleFire2: {
-      name: 'Kindle Fire 2',
-      styles: {
-        width: '600px',
-        height: '963px',
-      },
+  kindleFire2: {
+    name: "Kindle Fire 2",
+    styles: {
+      width: "600px",
+      height: "963px",
     },
-    kindleFireHD: {
-      name: 'Kindle Fire HD',
-      styles: {
-        width: '533px',
-        height: '801px',
-      },
+  },
+  kindleFireHD: {
+    name: "Kindle Fire HD",
+    styles: {
+      width: "533px",
+      height: "801px",
     },
-  };
+  },
+};
 
 // function loadStories() {
 //   require('../packages/components/modules/AdvancedSearch/__stories__/advancedSearch.stories.js')
@@ -34,17 +34,17 @@ const newViewports = {
 //   // You can require as many stories as you need.
 // }
 
-const req = require.context('../packages/components/modules',true,/.stories.js$/)
-function loadStories(){
-  req.keys().forEach(fileName => req(fileName))
+const req = require.context("../packages/components/src", true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach((fileName) => req(fileName));
 }
 
-addDecorator(withSmartKnobs)
-addDecorator(withKnobs)
-addDecorator(withPropsTable)
-addDecorator(withTests({results}));//jest
-addDecorator((storyFn, context) => withConsole()(storyFn)(context));//console
-addDecorator(withA11y)//ally
-addParameters({ viewport: newViewports });//view
+addDecorator(withSmartKnobs);
+addDecorator(withKnobs);
+addDecorator(withPropsTable);
+addDecorator(withTests({ results })); //jest
+addDecorator((storyFn, context) => withConsole()(storyFn)(context)); //console
+addDecorator(withA11y); //ally
+addParameters({ viewport: newViewports }); //view
 
 configure(loadStories, module);
