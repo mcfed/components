d6ee904880407dd83b0a42fd212e8b5ef07c3cd8
zhusiheng <zhusiheng@mchz.com.cn>
2020-06-29 19:29:20 +0800

    fix: 组件SwitchConfrmStory优化

--- src/SwitchConfirm/__stories__/SwitchConfirm.stories.js
+++ src/SwitchConfirm/__stories__/SwitchConfirm.stories.js
@@ -6,31 +6,44 @@ const stories = storiesOf('SwitchConfirm', module);
 
 stories.addParameters({jest: ['SwitchConfirm.spec.js']});
 
-stories.add('基础用法', () => (
-  <div>
-    <SwitchConfirm
-      modalConfirmProps={{title: 'title'}}
-      onConfirm={(current, option) => {
-        option();
-      }}
-    />
-    <SwitchConfirm
-      checked={true}
-      modalConfirmProps={{title: 'title'}}
-      onConfirm={(current, option) => {
-        option();
-      }}
-    />
-  </div>
-));
+class Show extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      currentText: props.currentOption || props.checked || false
+    };
+  }
+  handleSet(current) {
+    this.setState({
+      currentText: current
+    });
+  }
+  render() {
+    const {...SwitchProps} = this.props;
+    return (
+      <div>
+        <SwitchConfirm
+          {...SwitchProps}
+          onConfirm={(current, option) => {
+            console.log(current, this);
+            this.handleSet.call(this, current);
+            option();
+          }}
+        />
+        current:
+        <span>{String(this.state.currentText)}</span>
+      </div>
+    );
+  }
+}
+
+stories.add('基础用法', () => <Show modalConfirmProps={{title: 'title'}} />);
 stories.add('自定义ON|OFF用法', () => (
-  <SwitchConfirm
+  <Show
     uncheckedOption='OFF'
     checkedOption='ON'
     currentOption='OFF'
     modalConfirmProps={{title: 'title'}}
-    onConfirm={(current, option) => {
-      option();
-    }}
   />
 ));
