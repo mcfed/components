9a51a0fcac52e93cffe21b0a2ef0e7deacdeb0c5
Jshoes <576504437@qq.com>
2021-01-18 11:25:29 +0800

    feat: add components

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,5 @@ export {default as SwitchConfirm} from './SwitchConfirm';
 export {default as InputSearch} from './InputSearch';
 export {default as ToolBar} from './ToolBar';
 export {default as VerifyCode} from './VerifyCode';
+export {default as UploadFile} from './UploadFile';
+export {default as SelectInput} from './SelectInput';
