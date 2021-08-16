9136d5b90d7e0fc4858ee26e78b2ab6c0433540a
guosheng <guosheng@mchz.com.cn>
2021-08-16 19:17:35 +0800

    feat: 重构ts版Step组件

---
+++ src/TsStep/README.md
@@ -0,0 +1,85 @@
+# TsStep 组件 (目前为 Step 组件的 ts 重构版本，替换原先的 Step 组件)
+
+## Usage
+
+```tsx
+import React from 'react';
+import {Step} from '@mced/components';
+
+class FirstView extends React.Component {
+  render() {
+    return <div>first page</div>;
+  }
+}
+
+class SecondView extends React.Component {
+  render() {
+    return <div>second page</div>;
+  }
+}
+
+class ThirdView extends React.Component {
+  render() {
+    return <div>third page</div>;
+  }
+}
+
+const steps = [
+  {
+    text: 'firstStep',
+    description: 'firstStep',
+    path: 'first',
+    component: FirstView
+  },
+  {
+    text: 'secondStep',
+    description: 'secondStep',
+    path: 'second',
+    component: SecondView
+  },
+  {
+    text: 'thirdStep',
+    description: 'thirdStep',
+    path: 'third',
+    component: ThirdView
+  }
+];
+return (
+  <Step
+    className='McSteps'
+    showCancel={true}
+    backPath={`/xxx`}
+    steps={steps}
+    {...this.props}
+    showFinalLastStep={true}
+  />
+);
+```
+
+## Options
+
+| 参数                    | 说明                             | 类型                    | 默认值     |
+| ----------------------- | -------------------------------- | ----------------------- | ---------- |
+| steps                   | 步骤数组                         | array                   | []         |
+| cancelText              | 取消按钮自定义文字               | string                  | 取消       |
+| finishText              | 完成按钮自定义文字               | string                  | 完成       |
+| showPrev                | 是否展示上一步按钮               | boolean                 | true       |
+| finalSubmitFunctionName | 完成按钮点击回调方法名           | string                  | /          |
+| showFinalLastStep       | 是否在最后一个步骤展示上一步按钮 | string                  | true       |
+| showFinalCancel         | 是否在最后一个步骤展示取消按钮   | boolean                 | false      |
+| showCancel              | 是否展示取消按钮                 | boolean \| Function     | true       |
+| cancelConfirm           | 取消按钮二次确认 content         | string                  | 系统提示？ |
+| cancelConfirmTitle      | 取消按钮二次确认 title           | boolean                 | 确认执行？ |
+| backPath                | 取消按钮回退路径                 | string                  | /          |
+| btnLoading              | 按钮状态控制                     | BtnLoadingProps         | {}         |
+| customBtn               | 自定义按钮设置                   | ReactNode \| Function   | null       |
+| btnAlign                | 按钮布局                         | left \| center \| right | left       |
+
+## BtnLoadingProps
+
+| 参数   | 说明           | 类型    | 默认值 |
+| ------ | -------------- | ------- | ------ |
+| cancel | 取消按钮状态   | boolean | false  |
+| prev   | 上一步按钮状态 | boolean | false  |
+| next   | 下一步按钮状态 | boolean | false  |
+| finish | 完成按钮状态   | boolean | false  |
---
+++ src/TsStep/index.tsx
@@ -0,0 +1,294 @@
+import React, {Component, ReactNode} from 'react';
+import {Button, Steps, Modal} from 'antd';
+import StepsProps from 'antd/lib/steps/index';
+
+export const ref: any = React.createRef();
+
+type BtnLoadingProps = {
+  cancel: boolean;
+  prev: boolean;
+  next: boolean;
+  finish: boolean;
+};
+
+type BtnLoadingStatus = 'cancel' | 'prev' | 'next' | 'finish';
+
+export interface McStepProps extends StepsProps {
+  /**
+   * 步骤数组
+   */
+  steps: any[];
+  /**
+   * 取消按钮自定义文字
+   */
+  cancelText?: string;
+  /**
+   * 完成按钮自定义文字
+   */
+  finishText?: string;
+  /**
+   * 是否展示上一步按钮
+   */
+  showPrev?: boolean;
+  /**
+   * 完成按钮点击回调方法名
+   * 该方法名对应方法定义在步骤页面中
+   */
+  finalSubmitFunctionName?: string;
+  /**
+   * 是否在最后一个步骤展示上一步按钮
+   */
+  showFinalLastStep?: boolean;
+  /**
+   * 是否在最后一个步骤展示取消按钮
+   * 该属性是为了向前兼容，可以在最后一步放出取消按钮
+   */
+  showFinalCancel?: boolean;
+  /**
+   * 是否展示取消按钮
+   */
+  showCancel?: boolean | Function;
+  /**
+   * 取消按钮二次确认content
+   */
+  cancelConfirm?: string;
+  /**
+   * 取消按钮二次确认title
+   */
+  cancelConfirmTitle?: string;
+  /**
+   * 取消按钮回退路径
+   */
+  backPath?: string;
+  /**
+   * 按钮状态控制
+   */
+  btnLoading?: BtnLoadingProps;
+  /**
+   * 自定义按钮设置
+   */
+  customBtn?: ReactNode | Function;
+  /**
+   * 按钮布局
+   * left-靠左 center-居中 right-靠右
+   */
+  btnAlign?: 'left' | 'center' | 'right';
+  /**
+   * 父组件props属性
+   */
+  location: any;
+  /**
+   * 父组件props属性
+   */
+  history: any;
+  /**
+   *  样式类
+   */
+  // className?: string;
+}
+
+interface StepState {
+  currentIndex: number;
+  step: number;
+}
+
+export default class TsStep extends Component<McStepProps, StepState> {
+  state = {
+    currentIndex: 0,
+    step: 1
+  };
+
+  componentWillMount(): void {
+    const {location, steps = []} = this.props;
+    const currentStepRoute = `${location?.pathname?.slice(1)}${
+      location?.search
+    }`;
+
+    let currentStepNum = 1;
+    steps?.map((v: any, index: number) => {
+      /* istanbul ignore else */
+      if (currentStepRoute?.indexOf(v?.path) > -1) {
+        currentStepNum = index + 1;
+      }
+    });
+
+    this.setState({
+      currentIndex: currentStepNum - 1,
+      step: currentStepNum
+    });
+  }
+
+  goToStep = (step: number): void => {
+    const {history, steps} = this.props;
+    history?.push(steps[step - 1]?.path);
+    this.setState({
+      currentIndex: step - 1,
+      step
+    });
+  };
+
+  handleCancel(): void {
+    const {cancelConfirm, cancelConfirmTitle, backPath, history} = this.props;
+    if (cancelConfirm || cancelConfirmTitle) {
+      Modal.confirm({
+        title: cancelConfirmTitle || '系统提示？',
+        content: cancelConfirm || '确认执行？',
+        onOk: () => history.push(backPath)
+      });
+    } else {
+      history.push(backPath);
+    }
+  }
+
+  handleFinish(): void {
+    const {finalSubmitFunctionName} = this.props;
+    if (finalSubmitFunctionName) {
+      ref?.current && ref?.current[finalSubmitFunctionName]();
+    } else {
+      ref?.current?.onSubmit('handleSubmit');
+    }
+  }
+
+  getLoading(status: BtnLoadingStatus): boolean {
+    const {btnLoading} = this.props;
+    if (btnLoading && typeof btnLoading[status] === 'boolean') {
+      return btnLoading[status];
+    } else {
+      return false;
+    }
+  }
+
+  renderCancelBtn(): ReactNode | null {
+    const {
+      showCancel = true,
+      showFinalCancel = false,
+      steps,
+      cancelText
+    } = this.props;
+    const {currentIndex, step} = this.state;
+    if (showFinalCancel || step !== steps?.length) {
+      let flag: boolean = false;
+      if (typeof showCancel === 'boolean') {
+        flag = showCancel;
+      }
+      if (typeof showCancel === 'function') {
+        flag = showCancel(currentIndex);
+      }
+      return flag ? (
+        <Button
+          onClick={this.handleCancel.bind(this)}
+          loading={this.getLoading('cancel')}>
+          {cancelText || '取消'}
+        </Button>
+      ) : null;
+    } else {
+      return null;
+    }
+  }
+
+  renderCustomBtn(): ReactNode | null {
+    const {customBtn} = this.props;
+    const {currentIndex} = this.state;
+    if (typeof customBtn === 'function') {
+      return customBtn(currentIndex);
+    } else if (typeof customBtn === 'object') {
+      return customBtn;
+    } else {
+      return null;
+    }
+  }
+
+  renderPrev(): ReactNode | null {
+    const {steps, showPrev = true, showFinalLastStep = true} = this.props;
+    const {step} = this.state;
+    switch (step) {
+      case 1:
+        return null;
+      case steps?.length:
+        if (showPrev && showFinalLastStep) {
+          return (
+            <Button
+              onClick={() => this.goToStep(step - 1)}
+              loading={this.getLoading('prev')}>
+              {'上一步'}
+            </Button>
+          );
+        }
+      default:
+        if (showPrev) {
+          return (
+            <Button
+              onClick={() => this.goToStep(step - 1)}
+              loading={this.getLoading('prev')}>
+              {'上一步'}
+            </Button>
+          );
+        }
+    }
+    return null;
+  }
+
+  renderNextAndFinisah(): ReactNode {
+    const {steps, finishText} = this.props;
+    const {step} = this.state;
+    if (step === steps?.length) {
+      return (
+        <Button
+          onClick={this.handleFinish.bind(this)}
+          loading={this.getLoading('finish')}>
+          {finishText || '完成'}
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          onClick={() => ref?.current?.onSubmit('handleSubmit')}
+          loading={this.getLoading('next')}>
+          {'下一步'}
+        </Button>
+      );
+    }
+  }
+
+  renderStepButtonGroups() {
+    const {btnAlign = 'left'} = this.props;
+    return (
+      <Button.Group style={{textAlign: btnAlign}}>
+        {this.renderCancelBtn()}
+        {this.renderCustomBtn()}
+        {this.renderPrev()}
+        {this.renderNextAndFinisah()}
+      </Button.Group>
+    );
+  }
+
+  render() {
+    const {steps} = this.props;
+    const {step, currentIndex} = this.state;
+    /* istanbul ignore next */
+    const renderDom = steps[currentIndex] || steps[0];
+
+    return (
+      <React.Fragment>
+        <Steps current={currentIndex}>
+          {steps.map((item: any) => (
+            <Steps.Step
+              key={item?.text}
+              title={item?.text}
+              description={item?.description}
+            />
+          ))}
+        </Steps>
+
+        <renderDom.component
+          ref={ref}
+          {...this.props}
+          goToStep={this.goToStep}
+          goToNext={() => this.goToStep(step + 1)}
+        />
+
+        {this.renderStepButtonGroups()}
+      </React.Fragment>
+    );
+  }
+}
