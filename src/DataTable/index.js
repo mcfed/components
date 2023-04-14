de84fe2f911d379fe8befd21ef062c62e7481d64
shentaoliu <shentaoliu@126.com>
2023-04-14 16:11:30 +0800

    feat: 列表全选改造

--- src/DataTable/index.js
+++ src/DataTable/index.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Table, Icon, Checkbox, Button, Row, Col, Form} from 'antd';
+// import './style/index.less'
+
+const CHECK_TYPE = {
+  全选所有: 'checkAll',
+  反选所有: 'checkInvert',
+  全选当前页: 'checkCurAll',
+  反选当前页: 'checkCurInvert',
+  单选: 'checkOne'
+};
+const CHECK_DISABLED_CLASS = 'check-disabled'
 
 export class TableMenu extends Component {
   state = {
@@ -191,14 +201,21 @@ class DataTable extends Component {
   clear() {
     const {clearSelectRows} = this.props;
     clearSelectRows && clearSelectRows();
+    this.props.setCheckType('')
   }
 
   renderTableClear() {
-    const {showSelectClear, rowSelection, dataSource = []} = this.props;
-    const len =
+    const {showSelectClear, rowSelection, dataSource = [], page, checkType} = this.props;
+    let len =
       rowSelection && rowSelection.selectedRowKeys
         ? rowSelection.selectedRowKeys.length
         : 0;
+    if (checkType == CHECK_TYPE.全选所有) {
+      len = page?.total
+      if (!!this.props?.disabledCount) {
+        len = len - this.props?.disabledCount
+      }
+    }
     return dataSource.length > 0 && showSelectClear && rowSelection ? (
       <div className='checkedClear'>
         <span>已选 {len} 项</span>
@@ -214,6 +231,18 @@ class DataTable extends Component {
     ) : null;
   }
 
+  // 取两个数组的交集
+  getIntersection = (arr1, arr2) => {
+    const set1 = new Set(arr1);
+    return arr2.filter((item) => set1.has(item));
+  }
+
+  // 取出数组B中存在但数组A中不存在的元素
+  getDifference = (arrA, arrB) => {
+    const setA = new Set(arrA);
+    return arrB.filter((item) => !setA.has(item));
+  }
+
   render() {
     let {
       pagination,
@@ -222,6 +251,11 @@ class DataTable extends Component {
       defaultSort,
       showSelectClear,
       clearSelectRows,
+      selectedRowKeys,
+      setSelectedRowKeys,
+      checkType,
+      setCheckType,
+      checkAll,
       ...otherProps
     } = this.props;
     let {visible, columns} = this.state;
@@ -257,10 +291,94 @@ class DataTable extends Component {
         return it;
       });
     }
+    /**
+     * checkbox-全部选择功能支持
+     */
+    if (checkAll) {
+      this.props.rowSelection.hideDefaultSelections = true
+      this.props.rowSelection.selections = [
+        {
+          key: CHECK_TYPE.全选所有,
+          text: '全选所有',
+          onSelect: (changableRowKeys) => {
+            if (changableRowKeys?.length > 0) {
+              setSelectedRowKeys(changableRowKeys)
+              setCheckType(CHECK_TYPE.全选所有)
+            }
+          }
+        },
+        /** 目前【全选所有】的场景下可以全选当前页, 清空其他页面的选择，当前页面勾选保持不置灰 */
+        {
+          key: CHECK_TYPE.全选当前页,
+          text: '全选当前页',
+          onSelect: checkType == CHECK_TYPE.全选所有 ? (changableRowKeys) => {
+            // setSelectedRowKeys([])
+            // setSelectedRowKeys(changableRowKeys)
+            setCheckType(CHECK_TYPE.全选当前页)
+          } : (changableRowKeys) => {
+            let arr = []
+            arr = [...selectedRowKeys, ...changableRowKeys]
+            arr = Array.from(new Set(arr))
+            setSelectedRowKeys(arr)
+            setCheckType(CHECK_TYPE.全选当前页)
+          }
+        },
+        /** 目前只有【初始状态】或者【全选所有】的情况下才可以反选所有,其他情况暂不放开 */
+        {
+          key: CHECK_TYPE.反选所有,
+          text: <span className={`${checkType == CHECK_TYPE.全选所有 || checkType == CHECK_TYPE.反选所有 || checkType == '' ? '' : CHECK_DISABLED_CLASS}`}>反选所有</span>,
+          onSelect: (checkType == CHECK_TYPE.全选所有 || checkType == CHECK_TYPE.反选所有 || checkType == '' ) ? changableRowKeys => {
+            // 如果是初始状态(反选所有==全选所有)
+            // 如果是全选所有状态，则清空
+            if (checkType == '' || checkType == CHECK_TYPE.反选所有) {
+              setSelectedRowKeys(changableRowKeys)
+              setCheckType(CHECK_TYPE.全选所有)
+            } else if (checkType == CHECK_TYPE.全选所有) {
+              setSelectedRowKeys([])
+              setCheckType(CHECK_TYPE.反选所有)
+            }
+          } : null
+        },
+        /** 目前【全选所有】的场景下才不支持反选所有 */
+        {
+          key: CHECK_TYPE.反选当前页,
+          text: <span className={`${checkType == CHECK_TYPE.全选所有 ? CHECK_DISABLED_CLASS : ''}`}>反选当前页</span>,
+          onSelect: checkType == CHECK_TYPE.全选所有 ? null : changableRowKeys => {
+            // 当前页已选中的
+            let jiaoji = this.getIntersection(selectedRowKeys, changableRowKeys)
+            // 当前页未选中的
+            let chaji = this.getDifference(jiaoji, changableRowKeys)
+            // 所有的key，去除当前页已选中的，添加当前页未选中的
+            let arr = [];
+            arr = this.getDifference(jiaoji, selectedRowKeys)
+            arr = arr.concat(chaji)
+            setSelectedRowKeys(arr)
+            setCheckType(CHECK_TYPE.反选当前页)
+          }
+        }
+      ]
+      const onSelect = (record, selected, selectedRows) => {
+        let arr = [].concat(selectedRowKeys)
+        if (selected == true) {
+          arr.push(record?.id)
+        } else {
+          arr?.map((key, index) => {
+            if (key == record?.id) {
+              arr.splice(index, 1)
+            }
+          })
+        }
+        setCheckType(CHECK_TYPE.单选)
+        setSelectedRowKeys(arr)
+      }
+      this.props.rowSelection.onSelect = onSelect
+    }
+    
     // console.log("newColumns", newColumns);
     return (
       <div className='DataTable'>
         <Table
+          className={`${checkAll?'custom-table': ''}`}
           key={defaultSort && defaultSort.columnKey}
           {...otherProps}
           columns={newColumns}
@@ -299,4 +417,5 @@ DataTable.propTypes = {
   **/
   clearSelectRows: PropTypes.func
 };
+DataTable.CHECK_TYPE = CHECK_TYPE;
 export default DataTable;
